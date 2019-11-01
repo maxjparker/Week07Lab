1f0e290a0889aa183840d7f25f4211303226c33a
maximilian <45276378+scriptitator@users.noreply.github.com>
2019-11-01 12:18:05 -0600

    Week07Lab - RoleDB and service added

---
+++ src/java/services/RoleService.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package services;
+
+import java.util.ArrayList;
+import java.util.List;
+import models.Role;
+
+/**
+ *
+ * @author 657306
+ */
+public class RoleService
+{
+    public List<Role> getAll() {
+        ArrayList<Role> roles = new ArrayList<>();
+        //TODO: everything
+        return roles;
+    }
+    
+    public int insert(int roleID, String roleName) {
+        return -1;
+    }
+    
+    public int delete(int roleID) {
+        return -1;
+    }
+    
+    public int update(int roleID, String roleName) {
+        return -1;
+    }
+}
